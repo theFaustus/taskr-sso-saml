bfaf485ca3efe2a8524ca4cbe346cd5c8199d927
ipascari <ionpascari1995@gmail.com>
2020-10-31 22:17:29 +0200

    create the project skeleton

---
+++ src/main/java/com/evil/inc/taskrssosaml/TaskrSsoSamlApplication.java
@@ -0,0 +1,13 @@
+package com.evil.inc.taskrssosaml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskrSsoSamlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskrSsoSamlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/evil/inc/taskrssosaml/TaskrSsoSamlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evil.inc.taskrssosaml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskrSsoSamlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
